e5a21f2fad6bde2d6e0c543ae21c8923dc7d80d5
SergioRodriguez317 <sergioarg17@gmail.com>
2017-08-29 09:56:13 -0500

    Imagenes-Resources

--- PrototipoAntivirus/src/prototipoantivirus/Interfaz.java
+++ PrototipoAntivirus/src/prototipoantivirus/Interfaz.java
@@ -55,82 +55,82 @@ public class Interfaz extends javax.swing.JFrame {
         getContentPane().add(jLabel1);
         jLabel1.setBounds(10, 11, 70, 97);
 
-        jLabel2.setFont(new java.awt.Font("Comic Sans MS", 0, 24)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Century Gothic", 0, 24)); // NOI18N
         jLabel2.setText("Catch Defender");
         getContentPane().add(jLabel2);
         jLabel2.setBounds(226, 11, 367, 85);
 
         jButton1.setBackground(new java.awt.Color(255, 153, 255));
-        jButton1.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jButton1.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jButton1.setText("Seleccionar Archivo");
         jButton1.setName("btnSeleccionar"); // NOI18N
         getContentPane().add(jButton1);
-        jButton1.setBounds(10, 122, 165, 29);
+        jButton1.setBounds(10, 122, 180, 27);
 
-        jLabel3.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel3.setText("Contenido del Archivo");
         getContentPane().add(jLabel3);
-        jLabel3.setBounds(226, 126, 146, 21);
+        jLabel3.setBounds(226, 126, 160, 19);
         getContentPane().add(jTextField1);
         jTextField1.setBounds(218, 157, 164, 165);
 
         jButton2.setBackground(new java.awt.Color(255, 102, 255));
-        jButton2.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jButton2.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jButton2.setText("Analizar Archivo");
         jButton2.setName("btnAnalizar"); // NOI18N
         getContentPane().add(jButton2);
-        jButton2.setBounds(228, 345, 145, 29);
+        jButton2.setBounds(228, 345, 141, 27);
 
-        jLabel4.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel4.setText("Resultados del Análisis");
         getContentPane().add(jLabel4);
-        jLabel4.setBounds(440, 126, 155, 21);
+        jLabel4.setBounds(440, 126, 151, 19);
 
-        jLabel5.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel5.setText("¿Virus?");
         getContentPane().add(jLabel5);
-        jLabel5.setBounds(427, 166, 50, 21);
+        jLabel5.setBounds(427, 166, 46, 19);
 
         jTextField2.setName("txtVirus"); // NOI18N
         getContentPane().add(jTextField2);
         jTextField2.setBounds(520, 170, 93, 20);
 
-        jLabel6.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jLabel6.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel6.setText("Cantidad");
         getContentPane().add(jLabel6);
-        jLabel6.setBounds(427, 206, 59, 21);
+        jLabel6.setBounds(427, 206, 70, 19);
 
         jTextField3.setName("txtCantidad"); // NOI18N
         getContentPane().add(jTextField3);
         jTextField3.setBounds(520, 210, 93, 20);
 
-        jLabel7.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jLabel7.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel7.setText("Tipo Virus");
         getContentPane().add(jLabel7);
-        jLabel7.setBounds(454, 246, 68, 21);
+        jLabel7.setBounds(454, 246, 61, 19);
 
-        jLabel8.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jLabel8.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel8.setText("¿Usama?");
         getContentPane().add(jLabel8);
-        jLabel8.setBounds(427, 282, 60, 21);
+        jLabel8.setBounds(427, 282, 70, 19);
 
         jTextField4.setName("txtUsama"); // NOI18N
         getContentPane().add(jTextField4);
         jTextField4.setBounds(520, 280, 93, 20);
 
-        jLabel9.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jLabel9.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel9.setText("¿Amtrax?");
         getContentPane().add(jLabel9);
-        jLabel9.setBounds(427, 328, 67, 21);
+        jLabel9.setBounds(427, 328, 64, 19);
 
         jTextField5.setName("txtAmtrax"); // NOI18N
         getContentPane().add(jTextField5);
         jTextField5.setBounds(520, 330, 93, 20);
 
-        jLabel10.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        jLabel10.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel10.setText("¿éBola?");
         getContentPane().add(jLabel10);
-        jLabel10.setBounds(427, 369, 52, 21);
+        jLabel10.setBounds(427, 369, 60, 19);
 
         jTextField6.setName("txtEbola"); // NOI18N
         getContentPane().add(jTextField6);
@@ -139,11 +139,11 @@ public class Interfaz extends javax.swing.JFrame {
         jLabel11.setBackground(new java.awt.Color(255, 255, 255));
         jLabel11.setIcon(new javax.swing.ImageIcon("C:\\Users\\Sergio Rodriguez\\Documents\\Semestre 6\\Teoria de la computacion\\fondo de creta extra fino.jpg")); // NOI18N
         getContentPane().add(jLabel11);
-        jLabel11.setBounds(0, 0, 640, 110);
+        jLabel11.setBounds(0, 0, 630, 110);
 
         jLabel12.setIcon(new javax.swing.ImageIcon("C:\\Users\\Sergio Rodriguez\\Documents\\Semestre 6\\Teoria de la computacion\\fondo-abstracto-azul_1048-1511.jpg")); // NOI18N
         getContentPane().add(jLabel12);
-        jLabel12.setBounds(0, 110, 630, 320);
+        jLabel12.setBounds(0, 110, 626, 320);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
