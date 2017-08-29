b6d6edb2e96c99cd9f5c61204a504bd967dda59f
SergioRodriguez317 <sergioarg17@gmail.com>
2017-08-28 22:47:44 -0500

    Interfaz

--- PrototipoAntivirus/src/prototipoantivirus/PrototipoAntivirus.java
+++ PrototipoAntivirus/src/prototipoantivirus/PrototipoAntivirus.java
@@ -15,7 +15,7 @@ public class PrototipoAntivirus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
